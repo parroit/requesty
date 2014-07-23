5b42eed9dad71d6601a5e132adac4425bfc6667c
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-07-23 21:22:54 +0200

    custom reporter imroved

--- dist/client.js
+++ dist/client.js
@@ -1,135 +1,78 @@
-function FancyJSON(runner) {
+/*window.global_test_results = {
+  "passed": 1,
+  "failed": 3,
+  "total": 4,
+  "duration": 4321,
+  "tests": [
+    {
+      "name": "foo test",
+      "result": false,
+      "message": "sumthin bad",
+      "duration": 4000
+    },
+    {
+      "name": "bar test",
+      "result": false,
+      "message": "failure",
+      "duration": 300
+    },
+    {
+      "name": "baz test",
+      "result": true,
+      "message": "passed",
+      "duration": 20
+    },
+    {
+      "name": "qux test",
+      "result": false,
+      "message": "test bad",
+      "duration": 1
+    }
+  ]
+}*/
 
-    var root = null;
-    var result = {};
 
-    function recurse(suite, result) {
-
-        result.durationSec = 0;
-        result.passed = true;
-
-        if(suite.title)
-            result.description = suite.title;
-
-        if(suite.tests.length) {
-            result.specs = [];
-            for (var i = 0; i < suite.tests.length; i++) {
-
-                result.specs.push({
-                    "description": suite.tests[i].title,
-                    "durationSec": (suite.tests[i].duration / 1000) || 0, // duration of spec run in seconds
-                    "passed": suite.tests[i].state === "passed" // did the spec pass?
-                    //"passedCount": 1, // passed assertions in spec
-                    //"failedCount": 0, // failed assertions in spec
-                    //"totalCount": 1 // total assertions in spec
-                });
-
-                result.durationSec += (suite.tests[i].duration / 1000) || 0;
-
-                if(suite.tests[i].state !== "passed")
-                    result.passed = false;
-
-            }
-        }
-
-        if(suite.suites.length) {
-
-            result.suites = [];
-
-            var sub = null;
-            for (var j = 0; j < suite.suites.length; j++) {
-
-                sub = {};
-                recurse(suite.suites[j], sub);
-                result.suites.push(sub);
-
-                result.durationSec += sub.durationSec || 0;
-
-                if(!sub.passed)
-                    result.passed = false;
-
-            }
-
-        }
-
+function SauceReporter(runner) {
+    function makeTestResult(test, err) {
+        return {
+            name: test.fullTitle(),
+            result: (typeof err === 'undefined'),
+            message: err ? err.message : 'passed',
+            duration: 0
+        };
     }
 
-    runner.on('suite', function(suite) {
-        if(suite.parent.root) root = suite.parent;
+    var results = {
+        passed: 0,
+        failed: 0,
+        total: 0,
+        duration: 0,
+        tests: []
+    };
+
+    runner.on('pass', function(test) {
+        results.passed++;
+        results.total++;
+        var result = makeTestResult(test);
+        console.log(result);
+        results.tests.push(result);
+
+
+    });
+
+    runner.on('fail', function(test, err) {
+        results.failed++;
+        results.total++;
+
+        var result = makeTestResult(test, err);
+        console.log(result);
+        results.tests.push(result);
+
+
     });
 
     runner.on('end', function() {
-        recurse(root, result);
-        window.jsonReport = result;
+        window.global_test_results = results;
+
     });
-
-}
-
-
-function mochaSaucePlease(options, fn) {
-
-    (function(runner) {
-
-        // execute optional callback to give user access to the runner
-        if(fn) {
-            fn(runner);
-        }
-
-        if(!options) {
-            options = {};
-        }
-
-        // in a PhantomJS environment, things are different
-        if(!runner.on) {
-            return;
-        }
-
-        // Generate JSON coverage
-        mocha.reporter(FancyJSON);
-        new mocha._reporter(runner);
-
-        // Generate XUnit coverage
-        window.xUnitReport = 'off';
-        if(options.xunit != false) {
-            window.xUnitReport = '';
-            (function() {
-                var log = window.console && console.log;
-
-                if(!window.console) {
-                    window.console = {};
-                }
-
-                console.log = function() {
-                    window.xUnitReport += arguments[0] + "\n"; // TODO: handle complex console.log
-                    if(log) log.apply(console, arguments);
-                };
-            })();
-            mocha.reporter("xunit");
-            new mocha._reporter(runner);
-        }
-
-        // The Grid view needs more info about failures
-        var failed = [];
-        runner.on('fail', function(test, err) {
-            failed.push({
-                title: test.title,
-                fullTitle: test.fullTitle(),
-                error: {
-                    message: err.message,
-                    stack: err.stack
-                }
-            });
-        });
-
-        // implement custom reporter for console to read back from Sauce
-        runner.on('end', function() {
-            runner.stats.failed = failed;
-            runner.stats.xUnitReport = xUnitReport;
-            runner.stats.jsonReport = jsonReport;
-            window.mochaResults = runner.stats;
-            window.chocoReady = true;
-        });
-
-    })(window.mochaPhantomJS ? mochaPhantomJS.run() : mocha.run());
-
 }
